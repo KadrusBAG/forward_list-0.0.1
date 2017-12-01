b4ec6f642a2352866edc09a6551b2a980fd27f3c
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-12-01 11:53:41 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct node{
+    int val;
+    node * next;
+};
+
+void add(node * & head){
+    string stroka;
+    getline(cin, stroka);
+    istringstream stream(stroka);
+    int value;
+    stream >> value;
+    node * time = new node;
+    time->val = value;
+    time->next = nullptr;
+    if(head == NULL){
+        head = time;
+    }
+    else{
+        node * last = head;
+        while(last->next){
+            last = last->next;
+        }
+        last->next = time;
+    }
+}
+
+void sub(node * & head){
+    node * time = head;
+    head = head->next;
+    delete time;
+}
+
+void reverse(node * & head){
+    node * prev = nullptr, * cur = head->next, * next;
+	while (cur) {
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	head->next=cur;
+}
+
+void output(node * head){
+    node * time;
+	time = head;
+	while (time != NULL){
+		cout << "+---+" << "    ";
+		time = time->next;
+	}
+	time = head;
+	cout << endl;
+	while (time != NULL){
+		cout << "| " << time->val << " |";
+		time = time->next;
+		if (time != NULL){
+			cout << "--->";
+		}
+	}
+	time = head;
+	cout << endl;
+	while (time != NULL){ 
+		cout << "+---+" << "    ";
+		time = time->next;
+	}
+	cout << endl;
+}
+
+int main(){
+    node * head = new node;
+    head = nullptr;
+    char op;
+    while(cin >> op){
+        if(op == '+'){
+            add(head);
+        }
+        if(op == '-'){
+            sub(head);
+        }
+        if(op == '/'){
+            reverse(head);
+        }
+        if(op == '='){
+            output(head);
+        }
+        if(op == 'q'){
+            break;
+        }
+    }
+}
